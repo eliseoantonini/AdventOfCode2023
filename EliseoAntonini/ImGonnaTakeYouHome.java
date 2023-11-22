5a5266b6605421e72b60abe84fe8bd08c5084d80
GDI\antonini <antonini@gdi.de>
2023-11-22 16:56:48 +0100

    Java File Added

---
+++ EliseoAntonini/ImGonnaTakeYouHome.java
@@ -0,0 +1,8 @@
+public class ImGonnaTakeYouHome {
+
+    public final String COMPETITION_YEAR = "2023";
+
+    public static void main(String[] args) {
+          System.out.println("All the best and try to do your best"); 
+    }
+}
