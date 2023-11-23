84462e509e4da9f75675c6aaaa47fa99d1392c3a
GDI\antonini <antonini@gdi.de>
2023-11-23 17:16:58 +0100

    A new reading way for csv files

--- EliseoAntonini/Day_01.py
+++ EliseoAntonini/Day_01.py
@@ -1,5 +1,7 @@
 # AOfC_Day_01
 
+import csv
+
 class Game:
     def __init__(self, organisation, name, year):
         self.organization = organisation
@@ -11,15 +13,27 @@ class Game:
 
 game_01 = Game("GDI", "AdventOfCode", 2023)
 
-print(game_01.organization, ': ', game_01.name, game_01.year)
+# print(game_01.organization, ': ', game_01.name, game_01.year)
+# print(game_01)
 
-print(game_01)
-
-fileContent = open("EliseoAntonini\Data\input.txt", "r")
+#fileContent = open("EliseoAntonini\Data\input.txt", "r")
 # print(fileContent.read())
 
-for data in fileContent:
-    print(data)
+#for data in fileContent:
+#    print(data[3])
 
-fileContent.close()
+#fileContent.close()
+
+
+with open('EliseoAntonini\Data\input_feed.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
 
