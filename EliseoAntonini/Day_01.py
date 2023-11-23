ff65c3ce1e91344268aa6e71db89b4b616ac6369
GDI\antonini <antonini@gdi.de>
2023-11-23 16:38:04 +0100

    Class Game

--- EliseoAntonini/Day_01.py
+++ EliseoAntonini/Day_01.py
@@ -1,1 +1,16 @@
-# Day_01
+# AOfC_Day_01
+
+class Game:
+    def __init__(self, organisation, name, year):
+        self.organization = organisation
+        self.name = name
+        self.year = year
+
+    def __str__(self):
+        return f"{self.organization}({self.name}))"
+
+game_01 = Game("GDI", "AdventOfCode", 2023)
+
+print(game_01.organization, ': ', game_01.name, game_01.year)
+
+print(game_01)
