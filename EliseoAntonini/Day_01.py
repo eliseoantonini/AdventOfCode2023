4df6fd246cffcc34a36a310dc9cc646dd0b12fe1
GDI\antonini <antonini@gdi.de>
2023-11-24 08:29:06 +0100

    Input_feed uptdated

--- EliseoAntonini/Day_01.py
+++ EliseoAntonini/Day_01.py
@@ -30,7 +30,7 @@ with open('EliseoAntonini\Data\input_feed.txt') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            print(f'Column names are: {", ".join(row)}')
             line_count += 1
         else:
             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
