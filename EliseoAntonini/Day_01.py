8aa0d257c8f3592493682428ca8ea5f93254d78f
GDI\antonini <antonini@gdi.de>
2023-11-29 08:28:14 +0100

    Some I/O Experiments

--- EliseoAntonini/Day_01.py
+++ EliseoAntonini/Day_01.py
@@ -11,7 +11,15 @@ class Game:
     def __str__(self):
         return f"{self.organization}({self.name}))"
 
+class Renna:
+    def __init__(self, calories):
+        self.calories = calories
+
+
+
 game_01 = Game("GDI", "AdventOfCode", 2023)
+
+
 
 # print(game_01.organization, ': ', game_01.name, game_01.year)
 # print(game_01)
@@ -24,7 +32,7 @@ game_01 = Game("GDI", "AdventOfCode", 2023)
 
 #fileContent.close()
 
-
+'''
 with open('EliseoAntonini\Data\input_feed.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -36,10 +44,48 @@ with open('EliseoAntonini\Data\input_feed.txt') as csv_file:
             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
             line_count += 1
     print(f'Processed {line_count} lines.')
-
+'''
+'''
+switch = Switch()
+case_1 = lambda : "Eins"
+case_2 = lambda : "Zwei"
+case_3 = lambda : "Drei"
+'''
 
 with open('EliseoAntonini\Data\Calorie_Counting.txt', 'r') as calorieCount_file:
     csv_reader = csv.reader(calorieCount_file, delimiter=",")
-    for line in calorieCount_file.read().split('\n'):
-        print(line)
+    renna_1 = Renna(0)
+
+    #renna_2 = Renna(0)
+    #renna_3 = Renna(0)
+    #renna_4 = Renna(0)
+    #renna_5 = Renna(0)
+
+    dict = {
+      0: 'renna_1',
+      1: 'renna_2',
+      2: 'renna_3',
+      3: 'renna_4',
+      4: 'renna_4' 
+      }
+
     
+#def CountNumberOfAnimals():
+   
+    countNoData = 0
+    for line in calorieCount_file.read().split('\n'):
+            if (line) == '':
+                countNoData += 1
+                print(countNoData)
+    
+
+
+'''
+    print('Total number of renne:', countNoData + 1)
+    for line in calorieCount_file.read().split('\n'):
+        countNoData += 1    
+        if (line) != '': 
+            renna_1.calories += int(line);
+        else:
+            continue
+'''
